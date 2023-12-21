dfcd1765ec0e9f446a1a0f2361f637a63fee3d43
GaetanRaux <gaetan.raux@epitech.eu>
2023-12-21 09:52:54 +0100

    [ADD] ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,8 +1,20 @@
-
-
-
 import sys
 
-lines = []
-for line in sys.stdin:
-	lines.append(line.rstrip('\n'))
+def count_ingredients(lines):
+	current_ingredients = []
+	i = 1
+	while i <= int(lines[0]):
+		for ingredient in lines[i].split():
+			if (ingredient in current_ingredients) == False:
+				current_ingredients.append(ingredient)
+		i = i + 1
+	return len(current_ingredients)
+
+def main():
+	lines = []
+	for line in sys.stdin:
+		lines.append(line.rstrip('\n'))
+	print(count_ingredients(lines), end="")
+
+if __name__ == "__main__":
+    main()
